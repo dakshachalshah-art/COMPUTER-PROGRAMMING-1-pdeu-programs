f7b639f916b8a94e11db85443550e02dc823096d
dakshachalshah-art <dakshachalshah@gmail.com>
2026-08-11 17:32:03 +0530

    Add files via upload

---
+++ 178_2 sub.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+     int a,b,c;
+    printf("enter 2 nos,first greater than second ");
+    scanf("%d %d",&a,&b);
+    c=a-b;
+    printf("%d - %d = %d\n",a,b,c);
+    return 0;
+
+
+}
