9e1911afec87a5893c9804c7e9b9301590a615ac
dakshachalshah-art <dakshachalshah@gmail.com>
2026-08-11 17:08:54 +0530

    Add files via upload

---
+++ 178 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    float cel,fah;
+    printf("enter value of celsius");
+    scanf("%f",&cel);
+    fah=(9/5)*cel+32;
+    printf("value of fahrenheit =%f\n",fah);
+
+
+}
