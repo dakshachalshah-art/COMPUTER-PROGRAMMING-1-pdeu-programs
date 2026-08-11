3167099bfce7fd8510cc3f290b9003706ca5eaad
dakshachalshah-art <dakshachalshah@gmail.com>
2026-08-11 17:48:29 +0530

    Add files via upload

---
+++ 178_h to min.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float hrs,minutes;
+    printf("enter time in hours");
+    scanf("%f",&hrs);
+    minutes=hrs*60.0;
+    printf("time in minutes is=%f\n",minutes,hrs);
+}
